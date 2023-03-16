706d4f9196bf007cd611cce6f21cb6c6e616a1c5
Diego <diegoyon@outlook.com>
2023-03-16 16:36:41 -0600

    add bg color to not seen announcements

--- app/controllers/seen_announcements_controller.rb
+++ app/controllers/seen_announcements_controller.rb
@@ -1,6 +1,5 @@
 class SeenAnnouncementsController < ApplicationController
   def create
-    # debugger
     @seen_announcement = SeenAnnouncement.new(seen_announcement_params)
     if @seen_announcement.save
       flash[:notice] = "Mark as read."
@@ -13,9 +12,9 @@ class SeenAnnouncementsController < ApplicationController
   def edit; end
 
   def update
-    # debugger
     @seen_announcement = current_user.seen_announcements.find_by(announcement_id: params[:seen_announcement][:announcement_id])
     if @seen_announcement.update(seen_announcement_params)
+      flash[:notice] = "Updated"
       redirect_to announcements_path
     else
       render :edit, status: :unprocessable_entity
@@ -25,7 +24,6 @@ class SeenAnnouncementsController < ApplicationController
   private
 
   def seen_announcement_params
-    # debugger
     params.require(:seen_announcement).permit(:seen, :announcement_id).with_defaults(user_id: current_user.id)
   end
 end
