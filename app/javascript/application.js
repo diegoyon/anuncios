287fc4f86a06a6f7da78e964db360212c83b34a5
Diego <diegoyon@outlook.com>
2023-03-15 22:17:28 -0600

    add styles to navbar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,20 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
-import * as bootstrap from "bootstrap"
+import '@hotwired/turbo-rails';
+import './controllers';
+import * as bootstrap from 'bootstrap';
+
+// document.addEventListener('turbo:load', () => {
+//   var tooltipTriggerList = [].slice.call(
+//     document.querySelectorAll('[data-bs-toggle="tooltip"]')
+//   );
+//   var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+//     return new bootstrap.Tooltip(tooltipTriggerEl);
+//   });
+// });
+
+// let popoverTriggerList = [].slice.call(
+//   document.querySelectorAll('[data-bs-toggle="popover"]')
+// );
+// let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+//   return new bootstrap.Popover(popoverTriggerEl);
+// });
